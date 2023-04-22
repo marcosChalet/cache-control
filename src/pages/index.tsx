7920cc962cdf7cae8702fe8e400ce5528dee2b2a
marcosChalet <chaletmarcos@gmail.com>
2023-04-22 17:31:07 -0300

    feat: projeto base
    fix: habilitando CORS na vercel
    
    feat: adicionando cache-control

---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+import useSWR from "swr";
+
+const url = "https://api.adviceslip.com/advice/2";
+
+async function getAdvice() {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+export async function getServerSideProps(context) {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const advice = await res.json();
+
+  context.res.setHeader("Cache-Control", "max-age=86400");
+
+  return {
+    props: { advice },
+  };
+}
+
+export default function Home({ advice }) {
+  const { data: adviceSWR, error } = useSWR(url, getAdvice);
+
+  if (error) return <div>error...</div>;
+  if (!adviceSWR) return <div>loading...</div>;
+
+  return (
+    <main>
+      <p>{adviceSWR.slip.advice}</p>
+    </main>
+  );
+}
